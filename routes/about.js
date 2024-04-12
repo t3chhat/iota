91dbd05a7fab7146d3211a53caa4427309d10f7b
t3chhat <166677067+t3chhat@users.noreply.github.com>
2024-04-12 18:11:39 -0500

    Proj Rev 2

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about', { title: 'Iota' });
 });
 
 module.exports = router;
