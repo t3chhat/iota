6e905f1a952378b4a8bf6be3b7278a180515f1fb
t3chhat <166677067+t3chhat@users.noreply.github.com>
2024-06-09 01:09:03 -0500

    Rev 12

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Iota' });
-});
+  let query = "SELECT id, product_name, prod_image, description, price, size, color, brand_id, category_id, sold FROM product WHERE homepage = 1";
 
+  // execute query
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render('error');
+    }
+    res.render('index', {allrecs: result });
+  });
+});
+  
 module.exports = router;
