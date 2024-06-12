8c14b75be06f611c57fcf3b010a9d759be1e9d5c
t3chhat <166677067+t3chhat@users.noreply.github.com>
2024-06-12 04:05:11 -0500

    Rev Too Many

--- routes/catalog.js
+++ routes/catalog.js
@@ -13,5 +13,66 @@ router.get('/', function(req, res, next) {
         res.render('catalog', {allrecs: result });
     });
 });
+
+// ==================================================
+// Route to add an item to the cart
+// ==================================================
+    router.post('/add', function(req, res, next) {
+        if (typeof req.session.cart !== 'undefined' && req.session.cart ) {
+            if (req.session.cart.includes(req.body.id))
+            {
+                // Item Exists in Basket - Increase Quantity
+                var n = req.session.cart.indexOf(req.body.id);
+                req.session.qty[n] = parseInt(req.session.qty[n]) +
+                parseInt(req.body.qty);
+            }
+        else
+            {
+                // Item Being Added First Time
+                req.session.cart.push(req.body.id);
+                req.session.qty.push(req.body.qty);
+            }
+        }else {
+                var cart = [];
+                cart.push(req.body.id);
+                req.session.cart = cart;
+                var qty = [];
+                qty.push(req.body.qty);
+                req.session.qty = qty;
+            }
+    res.redirect('/catalog/cart');
+});
+    
+// ==================================================
+// Route to show shopping cart
+// ==================================================
+    router.get('/cart', function(req, res, next) {
+        if (!Array.isArray(req.session.cart) || !req.session.cart.length){
+            res.render('cart', {cartitems: 0 });
+        } else {
+            let query = "SELECT id, product_name, description, price, size, color, brand_id, category_id, sold, prod_image, homepage FROM product WHERE id IN (" + req.session.cart + ") order by find_in_set(id, '" + req.session.cart + "');";
+
+        // execute query
+        db.query(query, (err, result) => {
+            if (err) {res.render('error');} else
+                {res.render('cart', {cartitems: result, qtys: req.session.qty });}
+                });
+        }
+    });
+    
+
+// ==================================================
+// Route to remove an item from the cart
+// ==================================================
+    router.post('/remove', function(req, res, next) {
+        // Find the element index of the auto_id that needs to be removed
+    var n = req.session.cart.indexOf(req.body.id);
+        // Remove element from cart and quantity arrays
+    req.session.cart.splice(n,1);
+    req.session.qty.splice(n,1);
+    res.redirect('/catalog/cart');
+});
+    
+
 module.exports = router;
 
--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs');
+
+
+// ==================================================
+// Route Enable Registration
+// ==================================================
+router.get('/register', function(req, res, next) {
+    res.render('customer/addrec');
+    });
+
 
 // ==================================================
 // Route to list all records. Display view to list all records
@@ -41,7 +51,46 @@ router.get('/:recordid/show', function(req, res, next) {
 router.get('/addrecord', function(req, res, next) {
     res.render('customer/addrec');
     });
+
+// ==================================================
+// Route Provide Login Window
+// ==================================================
+router.get('/login', function(req, res, next) {
+    res.render('customer/login', {message: "Please Login"});
+    });
     
+    // ==================================================
+// Route Check Login Credentials
+// ==================================================
+router.post('/login', function(req, res, next) {
+    let query = "select id, first_name, last_name, password from customer WHERE username = '" + req.body.username + "'";
+    // execute query
+    db.query(query, (err, result) => {
+        if (err) {res.render('error');}
+        else {
+            if(result[0])
+                {
+                // Username was correct. Check if password is correct
+                bcrypt.compare(req.body.password, result[0].password, function
+                (err, result1) {
+                    if(result1) {
+                        // Password is correct. Set session variables for user.
+                        var custid = result[0].id;
+                        req.session.id = custid;
+                        var custname = result[0].first_name + " "+ result[0].last_name;
+                        req.session.custname = custname;
+                        res.redirect('/');
+                        } else {
+                    // password do not match
+                    res.render('customer/login', {message: "Wrong Password"});
+                }
+            });
+        }
+        else {res.render('customer/login', {message: "Wrong Username"});}
+        }
+    });
+});
+
 
 // ==================================================
 // Route to obtain customer input and save in database.
@@ -50,15 +99,38 @@ router.post('/', function(req, res, next) {
 
     let insertquery = "INSERT INTO customer (first_name, middle_name, last_name, email_address, phone_number, username, password, address_line1, address_line2, city, state, zip_code, country, payment_method, card_number, card_expiry, card_cvv) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    db.query(insertquery,[req.body.first_name, req.body.middle_name, req.body.last_name, req.body.email_address, req.body.phone_number, req.body.username, req.body.password, req.body.address_line1, req.body.address_line2, req.body.city, req.body.state, req.body.zip_code, req.body.country, req.body.payment_method, req.body.card_number, req.body.card_expiry, req.body.card_cvv],(err, result) => {
-        if (err) {
-            console.log(err);
-            res.render('error');
-        } else {
-            res.redirect('/customer');
-        }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+                db.query(insertquery,[req.body.first_name, req.body.middle_name, req.body.last_name, req.body.email_address, req.body.phone_number, req.body.username, hash, req.body.address_line1, req.body.address_line2, req.body.city, req.body.state, req.body.zip_code, req.body.country, req.body.payment_method, req.body.card_number, req.body.card_expiry, req.body.card_cvv],(err, result) => {
+                    if (err) {
+                            console.log(err);
+                            res.render('error');
+                        } else {
+                            res.redirect('/customer');
+                    }
+                });
+            });
     });
 });
+
+router.post('/', function(req, res, next) {
+    let insertquery = "INSERT INTO customer (first_name, middle_name, last_name, email_address, phone_number, username, password, address_line1, address_line2, city, state, zip_code, country, payment_method, card_number, card_expiry, card_cvv) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if(err) { res.render('error');}
+                db.query(insertquery,[req.body.first_name, req.body.middle_name, req.body.last_name, req.body.email_address, req.body.phone_number, req.body.username, req.body.address_line1, req.body.address_line2, req.body.city, req.body.state, req.body.zip_code, req.body.country, req.body.payment_method, req.body.card_number, req.body.card_expiry, req.body.card_cvv, hash],(err, result) => {
+                    if (err) {
+                        console.log(err);
+                        res.render('error');
+                    } else {
+                        res.redirect('/customer');
+                    }
+                });
+            });
+        });
+    });
 
 // ==================================================
 // Route to edit one specific record.
@@ -114,6 +186,6 @@ router.get('/:recordid/delete', function(req, res, next) {
     });
 });
     
-    
+
 
 module.exports = router;
