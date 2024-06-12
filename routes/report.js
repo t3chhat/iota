b79fc6c019a986991f8a9b51bae3c137513d3478
t3chhat <166677067+t3chhat@users.noreply.github.com>
2024-06-11 23:02:01 -0500

    Rev 13

--- routes/report.js
+++ routes/report.js
@@ -21,7 +21,47 @@ let query = "SELECT id, first_name, middle_name, last_name, email_address, phone
 			console.log(err);
 			res.render('error');
 		}
-	res.render('customer/allrecords', {allrecs: result });
+	res.render('report/customerlist', {allrecs: result });
     });
 });
 
+// ==================================================
+// Route to list all records. Display view to list all records
+// URL: http://localhost:3002/report/product
+// ==================================================
+
+router.get('/product', function(req, res, next) {
+    let query = "SELECT id, product_name, description, price, size, color, brand_id, category_id, sold, homepage FROM product"; 
+    
+        // execute query
+        db.query(query, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+        res.render('report/productlist', {allrecs: result });
+        });
+    });
+
+
+    // ==================================================
+    // Route to list all records. Display view to list all records
+    // URL: http://localhost:3002/report/sale
+    // ==================================================
+
+
+    router.get('/sale', function(req, res, next) {
+        let query = "SELECT od.id id, c.first_name first_name, c.last_name last_name, od.order_date order_date, p.product_name product_name, oi.price price, oi.quantity quantity FROM customer c, order_detail od, order_items oi, product p WHERE oi.order_id = od.id AND oi.product_id = p.id AND od.user_id = c.id"; 
+        
+            // execute query
+            db.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+            res.render('report/salelist', {allrecs: result }); 
+            });
+        });
+        
+
+module.exports = router; 
